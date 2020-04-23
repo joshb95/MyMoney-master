f537c88a735466305c090419d7cf6cba655c606e
Josh <joshjbarnes@hotmail.co.uk>
2020-04-23 11:19:44 +0100

    Re-Commit - Income and Outgoings button and activities added

---
+++ app/src/main/java/gerber/apress/esctransition/Outgoings.java
@@ -0,0 +1,13 @@
+package gerber.apress.esctransition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Outgoings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outgoings);
+    }
+}
